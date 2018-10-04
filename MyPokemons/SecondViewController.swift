c12e4f6e66f841c88b93cda7cca47e1af82e970f
Endre Synnes <synend16@student.westerdals.no>
2018-10-04 21:54:00 +0200

    Initial Commit

---
+++ MyPokemons/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyPokemons
+//
+//  Created by Endre Mikal Synnes on 04/10/2018.
+//  Copyright © 2018 Endre Mikal Synnes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
